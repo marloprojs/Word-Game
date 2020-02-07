b6cbe22033e6780c24753840c462a675d6e4f272
mate6483 <mate6483@colorado.edu>
2020-02-07 12:46:50 -0700

    letters around circle

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ class Game:
         self.height = 600
         self.width  = 800
         self.display = None
-        self.font = pygame.font.Font("freesansbold.ttf", 80)
+        self.letter_width = 80
+        self.font = pygame.font.Font("freesansbold.ttf", self.letter_width)
         self.midpoint = int(self.width/2), int(self.height/2)
+
+    def coordinates(self, angle, radius):
+        y = math.cos(angle)*radius
+        x = math.sin(angle)*radius
+        return (self.midpoint[0] + x - (self.letter_width/2), self.midpoint[1] - y - (self.letter_width/2))
+
 
     def setup(self):
         self.display = pygame.display.set_mode((self.width, self.height), pygame.HWSURFACE)
@@ -27,12 +34,13 @@ class Game:
 
     def render_letters(self, letters = ["A","B","C","D","E","F","G"]):
         rendered_letters = []
-        sep_angle = 360 / len(letters)
+        sep_angle = (2*math.pi) / len(letters)
         for li, letter in enumerate(letters):
             angle = sep_angle*li
             letter = letters[li]
+            location = self.coordinates(angle, 250)
             rendered_letter = self.font.render(letter, True, BLACK)
-            self.display.blit(rendered_letter, self.midpoint)
+            self.display.blit(rendered_letter, location)
 
     def run(self):
         while True:
