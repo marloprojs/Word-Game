a2b6648b2481b1b05700eb66e17e4a4177732d7f
Nick <nick.nickauer@gmail.com>
2020-02-08 10:57:02 -0700

    Opens blank window

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import pygame
 import hunspell
+
+class Game:
+    def __init__(self):
+        self.height = 600
+        self.width  = 800
+        self.display = None
+
+    def setup(self):
+        pygame.init()
+        self.display = pygame.display.set_mode((self.width, self.height), pygame.HWSURFACE)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+if __name__ == "__main__":
+    G = Game()
+    G.setup()
+    G.run()
